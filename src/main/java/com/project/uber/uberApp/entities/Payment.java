af417bd54ee87ad0282d56823fa1aa764132fa2c
Amardeep Singh <amardeepsingh2602@gmail.com>
2024-08-10 11:20:21 +0530

    Defining the Entities,Services and Repositories of UBER application

---
+++ src/main/java/com/project/uber/uberApp/entities/Payment.java
@@ -0,0 +1,30 @@
+package com.project.uber.uberApp.entities;
+
+import com.project.uber.uberApp.entities.enums.PaymentMethod;
+import com.project.uber.uberApp.entities.enums.PaymentStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Ride ride;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @CreationTimestamp
+    private LocalDateTime paymentTime;
+}
